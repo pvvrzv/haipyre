f0cda55f4f0569750f79d6586632ab4edfb8a84d
pvvrzv <pvvrzv@gmail.con>
2022-08-14 18:02:03 +0300

    added addShadow method. Changed findLastMethod so it actually returns last and not one before it

--- src/utils/tree.js
+++ src/utils/tree.js
@@ -1,18 +1,23 @@
 import { ListNode, List } from './list.js';
 
 export class TreeNode extends ListNode {
-  constructor(children = null) {
+  constructor(children = null, shadow = null) {
     super();
     this.parent = null;
     this.children = children;
+    this.shadow = shadow;
   }
 
   addChild(node) {
-    if (!this.children) {
-      this.children = new List();
-    }
+    if (!this.children) this.children = new List();
     node.parent = this;
     this.children.push(node);
+  }
+
+  addShadow(node) {
+    if (!this.shadow) this.shadow = new List();
+    node.parent = this;
+    this.shadow.push(node);
   }
 
   getLastChild() {
@@ -34,10 +39,16 @@ export class TreeNode extends ListNode {
     return false;
   }
 
+  findLast(predicate) {
+    if (!this.children) return this;
+    const next = this.children.find(predicate);
+    if (next === undefined) return this;
+    return next.findLast(predicate);
+  }
 }
 
-export class Tree {
+export class Tree extends TreeNode {
   constructor() {
-    this.root = new TreeNode();
+    super();
   }
 }
